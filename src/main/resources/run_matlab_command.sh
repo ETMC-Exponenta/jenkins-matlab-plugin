6cc57d8e2569d17423a41f840f035414675fd052
Pavel Roslovets <roslovets@exponenta.ru>
2020-10-28 11:43:06 +0300

    Use local run-matlab-command.sh
    Old releases and Windows are not supported from now

---
+++ src/main/resources/run_matlab_command.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Executes a specified MATLAB command in batch mode
+
+usage() {
+    echo ''
+    echo '    Usage: run_matlab_command.sh command'
+    echo ''
+    echo '    command       - MATLAB script, statement, or function to execute.'
+    echo ''
+}
+
+command=$1
+if [ -z "$command" ]; then
+    usage
+    exit 1
+fi
+
+matlab -batch "$command"
